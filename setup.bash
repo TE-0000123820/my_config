b55ef77eabd6851dbc5e42b1b80ea42db22074b8
Norito Kato <Norito.Kato@sony.com>
2022-11-08 17:57:06 +0900

    Refactor and add "mkdir -p ~/.cache/shell/"

--- setup.bash
+++ setup.bash
@@ -4,30 +4,50 @@ set -eu -o pipefail
 #
 # vim
 #
+function setup_vim() {
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 mkdir -p ~/vim_swap
+}
 
 #
 # tmux
 #
+function setup_tmux() {
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
 
 #
 # python
 #
+function setup_python() {
 sudo apt install python3-pip
 pip3 install --user neovim python-lsp-server
+}
 
 #
 # github
 #
+function setup_github() {
 cat <<EOF >> ~/.ssh/config
 Host github.com
     User git
     HostName ssh.github.com
     Port 443
 EOF
+}
+
+#
+# misc
+#
+function setup_misc() {
+mkdir -p ~/.cache/shell/
+}
+
+setup_vim
+setup_tmux
+setup_python
+setup_github
+setup_misc
